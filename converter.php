5aeed396010968e9be04c3dba650a1f17fbfccc1
Oscar Dealtry <odealtry@gmail.com>
2021-05-11 16:31:29 +0100

    basic communication between files

--- converter.php
+++ converter.php
@@ -3,6 +3,15 @@
     <title>PHP Test</title>
   </head>
   <body>
-    Your numeral is <?php echo $_POST["numeral"]; ?>
+    Your numeral is <?php echo $_POST['numeral']; ?>
+    <br>
+    <?php
+    $numeral = $_POST['numeral'];
+    if (strlen($numeral) > 2) {
+      echo "Cor that's complicated m8";
+    } else {
+      echo "now this we may be able to handle";
+    }
+    ?>
   </body>
 </html>
