efe0dc61de682e559f6ee8050b6428ead9113af5
Oscar Dealtry <odealtry@gmail.com>
2021-05-11 16:16:36 +0100

    first commit

---
+++ converter.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    Your numeral is <?php echo $_POST["numeral"]; ?>
+  </body>
+</html>
---
+++ experiments.php
@@ -0,0 +1,26 @@
+<?php
+// function greet ($name) {
+  // echo "What the fuk is up $name!!!!";
+// }
+
+// greet("Oscar");
+
+phpinfo();
+
+$array1 = [
+  "foo" => "bar",
+  "second" => "entry",
+];
+
+$array2 = ["foo", "bar"];
+
+// var_dump($array1);
+// var_dump($array2);
+
+function print_array ($array) {
+  var_dump($array);
+}
+
+print_array($array2);
+
+?>
